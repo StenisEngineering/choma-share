49b5dd5f287373ab2383be93bb5997a1af8e99fd
Stenis <engineeringstenis@gmail.com>
2026-05-11 21:24:00 +0100

    Fix React not defined crash

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode, useState } from 'react'
+import React, { StrictMode, useState } from 'react'
 import { createRoot }  from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider, useAuth } from './hooks/useAuth'
